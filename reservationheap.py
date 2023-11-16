5b826e1677ba6c5002bc5f39f2dd8383b02a3dfc
g-an24 <pratham.sharma6113@gmail.com>
2023-11-16 15:21:20 -0500

    manual_heap_function

--- reservationheap.py
+++ reservationheap.py
@@ -1,13 +1,12 @@
-import heapq
 from time import time
 
 class Reservation:
     def __init__(self, patronID, bookID, priorityNumber, timestamp=None):
         self.patronID = patronID
-        self.bookID = bookID  # Add this line to include bookID in the reservation
+        self.bookID = bookID
         self.priorityNumber = priorityNumber
-        self.timestamp = timestamp if timestamp else time()
-    
+        self.timestamp = timestamp if timestamp is not None else time()
+
     def __lt__(self, other):
         if self.priorityNumber == other.priorityNumber:
             return self.timestamp < other.timestamp
@@ -16,61 +15,63 @@ class Reservation:
     def __repr__(self):
         return f"({self.patronID}, {self.bookID}, {self.priorityNumber}, {self.timestamp})"
 
+class WaitlistFullException(Exception):
+    """Exception raised when the waitlist is full."""
+    pass
 
 class ReservationHeap:
     def __init__(self):
         self.heap = []
         self.max_size = 20
-        self.index_map = {}  # Maps patronID to the index in the heap
+
+    def _heapify_up(self, index):
+        while index > 0:
+            parent = (index - 1) // 2
+            if self.heap[index] < self.heap[parent]:
+                self.heap[index], self.heap[parent] = self.heap[parent], self.heap[index]
+                index = parent
+            else:
+                break
+
+    def _heapify_down(self, index):
+        length = len(self.heap)
+        while index * 2 + 1 < length:
+            smallest = index
+            left = index * 2 + 1
+            right = index * 2 + 2
+
+            if self.heap[left] < self.heap[smallest]:
+                smallest = left
+
+            if right < length and self.heap[right] < self.heap[smallest]:
+                smallest = right
+
+            if smallest != index:
+                self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
+                index = smallest
+            else:
+                break
 
     def insert(self, reservation):
         if len(self.heap) >= self.max_size:
-            raise Exception("Waitlist is full")
-        self.index_map[reservation.patronID] = len(self.heap)
-        heapq.heappush(self.heap, reservation)
+            raise WaitlistFullException("Waitlist is full")
+        self.heap.append(reservation)
+        self._heapify_up(len(self.heap) - 1)
 
     def extract_min(self):
-        if self.heap:
-            # When popping, remove the patronID from the index map
-            reservation = heapq.heappop(self.heap)
-            del self.index_map[reservation.patronID]
-            # Rebuild index map for all items since they could have been reordered
-            self._rebuild_index_map()
-            return reservation
-        return None
+        if not self.heap:
+            return None
+        smallest = self.heap[0]
+        self.heap[0] = self.heap[-1]
+        self.heap.pop()
+        self._heapify_down(0)
+        return smallest
 
     def peek(self):
-        if self.heap:
-            return self.heap[0]
-        return None
-
-    def remove(self, patronID):
-        index = self.index_map.get(patronID)
-        if index is not None:
-            # Swap with the last reservation and update the index map
-            self.index_map[self.heap[-1].patronID] = index
-            self.heap[index] = self.heap[-1]
-            self.heap.pop()
-            # Rebuild the heap from the new index and remove the patronID from the map
-            if index < len(self.heap):
-                heapq._siftup(self.heap, index)
-                heapq._siftdown(self.heap, 0, index)
-            del self.index_map[patronID]
-            # Rebuild index map for all items since they could have been reordered
-            self._rebuild_index_map()
-
-    def update(self, patronID, newPriorityNumber):
-        for reservation in self.heap:
-            if reservation.patronID == patronID:
-                reservation.priorityNumber = newPriorityNumber
-                heapq.heapify(self.heap)
-                self._rebuild_index_map()
-                break
+        return self.heap[0] if self.heap else None
 
     def print_heap(self):
-        print("Current waitlist:")
         for reservation in self.heap:
             print(reservation)
 
-    def _rebuild_index_map(self):
-        self.index_map = {reservation.patronID: i for i, reservation in enumerate(self.heap)}
+
